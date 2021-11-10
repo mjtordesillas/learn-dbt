3f4b5637a7e5f5f3be9a3193abf1332670291d3f
manuel.tordesillas@lifullconnect.com <manuel.tordesillas@lifullconnect.com>
2021-11-10 07:16:36 +0000

    extracted customers module

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -2,18 +2,6 @@
     materialized="table"
 )}}
 
-
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
-),
-
 orders as (
 
     select
